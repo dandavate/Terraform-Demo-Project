d70d8bd7610321ce2cdc275ed1bbe43d3e159c80
Ganesh_Dandavate <dandavate000@gmail.com>
2023-02-03 17:11:24 +0530

    added internet gatway and route table for external communication

--- main.tf
+++ main.tf
@@ -14,13 +14,13 @@ provider "aws" {
     region = "us-east-1"
 }
 
-#sets variables
+#defined variables
 variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
 
-#create custome vpc
+#creates custome vpc
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
@@ -29,7 +29,7 @@ resource "aws_vpc" "myapp-vpc" {
     }
 }
 
-#Create subnet-1 inside myapp-vpc
+#Creates subnet-1 inside myapp-vpc
 resource "aws_subnet" "myapp-subnet-1" {
     vpc_id = aws_vpc.myapp-vpc.id
     cidr_block = var.subnet_cidr_block[0]
@@ -38,4 +38,29 @@ resource "aws_subnet" "myapp-subnet-1" {
         Name = "${var.env_prefix[0]}-subnet-1"
         environmet = var.env_prefix[1]
     }
+}
+
+# creates internet getway
+resource "aws_internet_gateway" "myapp-internet-gateway" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name = "${var.env_prefix[0]}-igw"
+        environmet = var.env_prefix[1]
+    }
+}
+
+#Create route table
+# internal route will create automatically
+# define external route
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-internet-gateway.id
+    }
+    tags = {
+        Name = "${var.env_prefix[0]}-rtb"
+        environmet = var.env_prefix[1]
+    }
+  
 }
