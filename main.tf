e5cce49be3c7fcbb8a9c0ab0885b872b68caf366
Ganesh_Dandavate <dandavate000@gmail.com>
2023-02-03 18:15:22 +0530

    added routes into default myapp route table

--- main.tf
+++ main.tf
@@ -49,3 +49,16 @@ resource "aws_internet_gateway" "myapp-internet-gateway" {
     }
 }
 
+# get default vpc object using this command $ terraform state show aws_vpc.myapp-vpc    
+resource "aws_default_route_table" "myapp-default-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+     route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-internet-gateway.id
+    }
+    tags = {
+        Name = "${var.env_prefix[0]}-default-rtb"
+        environmet = var.env_prefix[1]
+    }
+}
+
