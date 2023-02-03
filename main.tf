d7c07cb3f6cbbce5c2060a918442ca936bdaad7c
Ganesh_Dandavate <dandavate000@gmail.com>
2023-02-03 15:33:10 +0530

    added custom vpc and subnet

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+#defined providers
+
 terraform {
   required_providers {
     aws = {
@@ -7,6 +9,31 @@ terraform {
   }
 }
 
+#accessing providers
 provider "aws" {
-  # Configuration options
+    region = "us-east-1"
+}
+
+#sets variables
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
+
+#create custome vpc
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        Name = "${var.env_prefix}-vpc"
+    }
+}
+
+#Create subnet-1 inside myapp-vpc
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block[0]
+    availability_zone = var.avail_zone[0]  
+    tags = {
+        Name = "${var.env_prefix}-subnet-1"
+    }
 }
