b541a0bc1b9809709563bc8916ecece7da3f62df
Ganesh_Dandavate <dandavate000@gmail.com>
2023-02-04 15:48:55 +0530

    created instance and user data

--- main.tf
+++ main.tf
@@ -94,5 +94,51 @@ resource "aws_default_security_group" "default-myapp-sg" {
     }
 }
 
+# fetch AMI 
+data "aws_ami" "latest-ami" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = [ "amzn2-ami-kernel-*-x86_64-gp2" ]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+# create ssh-key
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = "${file(var.public_key_location)}"
+  
+}
+
+# create instance
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-ami.id
+    instance_type = var.instance_type
+    
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-myapp-sg.id]
+
+    availability_zone = var.avail_zone[0]
+    associate_public_ip_address = true
+
+    key_name = aws_key_pair.ssh-key.key_name
+
+    user_data = file("user-data-script.sh")
+                                  
+    tags = {
+         Name = "${var.env_prefix[0]}-server"
+         environmet = var.env_prefix[1]
+    }
+}
+
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip
+}
+
 
 
